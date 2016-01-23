ef1b645c2bc15a69a1710dc3b606695437c92607
mlchow <marisalchow@gmail.com>
2016-01-23 17:32:48 -0500

    does a decent job of random walk over the visible screen

--- spriteAnimate/testAnimation/page.js
+++ spriteAnimate/testAnimation/page.js
@@ -1,15 +1,63 @@
 //Global variables that will be accessed in the functions below.
 var currentKey;          //records the current key pressed
 var TimerWalk;          //timer handle
+var limit = 7;        // max number of steps to take in a direction
 var charStep = 2;       //1=1st foot, 2=stand, 3=2nd foot, 4=stand
-var charSpeed = 400; //how fast the character will move
+var charSpeed = 750; //how fast the character will move
+var prevStep = 'down';
+var hitBounds = false;
 
+/*
+ * Set of functions which give the opposite or adjacent
+ * moves of the given move. 
+ * @move The given move.
+ */
+function opposite(move) {
+  if (move == "up") return "down";
+  if (move == "down") return "up";
+  if (move == "left") return "right";
+  if (move == "right") return "left";
+}
+function adj1(move) {
+  if (move == "up") return "left";
+  if (move == "down") return "left";
+  if (move == "left") return "up";
+  if (move == "right") return "up";
+}
+function adj2(move) {
+  if (move == "up") return "right";
+  if (move == "down") return "right";
+  if (move == "left") return "down";
+  if (move == "right") return "down";
+}
+
+/* 
+ * When the document loads, start moving the sprite. 
+ */
 $(document).ready(function() {
 	//add character state class
 	$('#character').addClass('front-stand');
-  console.log("added");
-});
 
+  setInterval(function(){
+    var r = Math.random();
+
+    if (r < 0.10 || hitBounds) {
+      charWalk(opposite(prevStep));
+     
+      prevStep = opposite(prevStep);
+      hitBounds = false;
+    } else if (r < 0.76) {
+      charWalk(prevStep);
+    } else if (r < 0.88) {
+      charWalk(adj1(prevStep));
+      prevStep = adj1(prevStep);
+    } else {
+      charWalk(adj2(prevStep));
+      prevStep = adj2(prevStep);
+    }
+  }, 750);
+});
+/*
 //KeyDown Function
   //if there is no currentKey down, execute charWalk
 $(document).keydown(function(e) {
@@ -17,7 +65,6 @@ $(document).keydown(function(e) {
  
       //set the currentKey to the key that is down
       currentKey = e.keyCode;
-      console.log(e.keyCode);
  
       //execute character movement function charWalk('direction')
       switch(e.keyCode) {
@@ -49,7 +96,7 @@ $(document).keydown(function(e) {
  
     }
  
-  });
+  });*/
 
   //Character Walk Function
   function charWalk(dir) {
@@ -59,30 +106,34 @@ $(document).keydown(function(e) {
     if (dir == 'down') dir = 'front';
  
     //move the character
-    processWalk(dir);
- 
-    //set the interval timer to continually move the character
-    TimerWalk = setInterval(function() { processWalk(dir); }, charSpeed);
- 
+    processWalk(dir);  
   }
  
   //Process Character Walk Function
   function processWalk(dir) {
+    var t = $('#character').offset().top;
+    var l = $('#character').offset().left;
+    if ((dir == "back" && t-20<0) ||
+        (dir == "front" && t+68>$(window).height()) ||
+        (dir == "left" && l-20<0) ||
+        (dir == "right" && l+52>$(window).width())) {
+      hitBounds = true;
+    }
  
     //increment the charStep as we will want to use the next stance in the animation
    //if the character is at the end of the animation, go back to the beginning
     charStep++;
-    if (charStep == 5) charStep = 1;
+    if (charStep == 3) charStep = 1;
  
     //remove the current class
     $('#character').removeAttr('class');
  
     //add the new class
     switch(charStep) {
-      case 1: $('#character').addClass(dir+'-stand'); break;
-      case 2: $('#character').addClass(dir+'-right'); break;
-      case 3: $('#character').addClass(dir+'-stand'); break;
-      case 4: $('#character').addClass(dir+'-left');  break;
+      //case 1: $('#character').addClass(dir+'-stand'); break;
+      case 1: $('#character').addClass(dir+'-right'); break;
+      //case 3: $('#character').addClass(dir+'-stand'); break;
+      case 2: $('#character').addClass(dir+'-left');  break;
     }
  
     //move the char
