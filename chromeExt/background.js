a7e01b75ca64a253c72d5c0fb407d293666ccd90
Eugene Tang <eugenetang@princeton.edu>
2016-01-23 09:50:08 -0500

    update webserver

--- chromeExt/background.js
+++ chromeExt/background.js
@@ -1,17 +1,3 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // No tabs or host permissions needed!
-  console.log('Turning ' + tab.url + ' red!');
-  chrome.tabs.executeScript({
-    code: 'document.body.style.backgroundColor="red"'
-  });
-  chrome.extension.getBackgroundPage().console.log('foo');
-});
-
 var socket = io.connect('http://192.241.182.93:3000/');
 //socket.emit('chat message', 'blue');
 socket.on('chat message', function(msg){
--- webserver/index.js
+++ webserver/index.js
@@ -7,6 +7,8 @@ mongoose.connect("mongodb://user:password@ds037215.mongolab.com:37215/cybertroll
 var Schema = mongoose.Schema;
 var UserSchema = new Schema({
   name: { type: String, required: true, dropDups: true },
+  online: { type: Boolean, required: true },
+  friends: [{ type: String, dropDups: true }]
 });
 var User = mongoose.model('User', UserSchema);
 
@@ -14,9 +16,32 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+// create new user
 app.get('/createuser/:username', function(req, res) {
+	User.findOne({ 'name': req.params.username }, 'name', function (err, person) {
+		if (err) { console.log(err); return }
+		if (person !== null) {
+			res.send('Username already taken')
+			return
+		}
+	})
 	var new_user = new User({name: req.params.username})
-	new_user.findOne({ 'name': req.params.username })
+	new_user.save(function(err) {
+		if (err) { console.log(err); return }
+		res.send('Success')
+	})
+})
+
+// add friend
+app.get('/addfriend/:username', function(req, res) {
+	User.findOne({ 'name': req.params.username }, 'name', function (err, person) {
+		if (err) { console.log(err); return }
+		if (person === null) {
+			res.send('User does not exist')
+			return
+		}
+	})
+
 	new_user.save(function(err) {
 		if (err) { console.log(err); return }
 		res.send('Success')
