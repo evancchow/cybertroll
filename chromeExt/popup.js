58e7ed007be0b4bac3e6f9ff41e2efc6d7f47596
Evan Chow <echow@princeton.edu>
2016-01-23 17:28:23 -0800

    added an add friend input

--- chromeExt/popup.js
+++ chromeExt/popup.js
@@ -87,7 +87,7 @@ function startup(username) {
 
    /* Add friend button */
   $('#addfriendbutton').click(function () {
-    var newFriend = window.prompt("Enter the name of your friend to add: ", "Albert Einstein");
+    var newFriend = $('#add_friend_input').val().toLowerCase();
     addFriend(username, newFriend); // username here works
   });
 
@@ -104,6 +104,7 @@ function startup(username) {
       window.location.href = "login.html";
     });
   });
+
 };
 
 function httpGet(theUrl, callback) {
