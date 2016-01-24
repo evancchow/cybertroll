ea2c7f50fb435a2b423bc4a2465551e4affb1b71
Connor Anderson <connora@stanford.edu>
2016-01-23 20:07:34 -0500

    its pretty now

--- spriteAnimate/testAnimation/js/blast_all.js
+++ spriteAnimate/testAnimation/js/blast_all.js
@@ -4,31 +4,12 @@
 /* jquery.blast.js */
 
 
-
-
-
-// blastWebpage_Words();
-// console.log("webpage blasted");
-// deleteChar();
-// blockWord();
-// generateBlocks();
-// screenCapture();
-// blastWebpage_Chars()
-// deleteChar();
-// console.log("chars deleted");
-// deleteElem();
-
-// blastWebpage_Words_Divs(globalPositionAll);
-
-// blastWebpage_Chars_Divs(globalPositionAll);
-// deleteEveryOtherChar();
-
 function blastWebpage(){ // choose which mode you want
   blastWebpage_Chars_Divs(globalPositionAll);
 }
 
 
-
+/* Blasts by word division */
 function blastWebpage_Words_Divs(callback){
 
   $("div")
@@ -63,18 +44,16 @@ function blastWebpage_Words_Divs(callback){
   callback(x, divPositions);
 
 }
-
+/* Blasts by char division */
 function blastWebpage_Chars_Divs(callback){
 $("span").blast(false);
 $("div")
-  // Blast the text apart by char.
   .blast({ 
     delimiter: "character", // Set the delimiter type (see left)
     tag: "span", // Set the wrapping element type (e.g. "div")
     });
 
   $("p")
-  // Blast the text apart by word.
   .blast({ 
     delimiter: "character", // Set the delimiter type (see left)
     tag: "span", // Set the wrapping element type (e.g. "div")
@@ -137,27 +116,18 @@ function globalPositionAll(x, divPositions){
 
 }
 
-function deleteEveryOtherChar(){
-
-	var background_rgb = [255, 255, 255];
-    $("span").each(function(index, element) {
-
-	if(index %2 == 0){
-		deleteDivElem($(this));
-	}
-}); 
-}
-
-
+/* Delete an Element */
 function deleteElem(element){
   element.remove();
 }
 
+/* Move an Element */
 function moveDivElem(element,newX,newY){
   testdiv.style.left =  newX; 
   testdiv.style.top = newY; 
 }
 
+/* Get an Element's Position */
 function GetDivPositioning(element){
   testdiv.style.left =  curX; 
   testdiv.style.top = curY; 
@@ -165,26 +135,7 @@ function GetDivPositioning(element){
   return position;
 }
 
-function deleteElem(element){
-
-	var background_rgb = [255, 255, 255];
-
-	var c = $("body").css("background-color");
-	var colors = c.replace(/^rgba?\(|\s+|\)$/g,'').split(',');
-	background_rgb[0] = colors[0];
-	background_rgb[1] = colors[1];
-	background_rgb[2] = colors[2];
-
-	if(background_rgb[0] == background_rgb[1] == background_rgb[2] == 0){ // Handle if background not defined or black
-		background_rgb[0] = 255;
-		background_rgb[1] = 255;
-		background_rgb[2] = 255;
-	}
-
-	var backgroundWrite = "rgb(" + background_rgb[0] + ", " + background_rgb[1] + ", " + background_rgb[2] + ")";
-	element.css("color" , backgroundWrite);
-}
-
+/* Get screen capture at a div */
 function screenCapture(element){
 $(document).ready(function() {
     // var testdiv = document.getElementById("blast-1");
@@ -199,89 +150,3 @@ $(document).ready(function() {
     console.log(" Screen capture complete");
   });
 }
-
-// SAMPLE DIV BLASTING
-// $("div")
-//   // Blast the text apart by word.
-//   .blast({ 
-//   	delimiter: "word", // Set the delimiter type (see left)
-//     tag: "div", // Set the wrapping element type (e.g. "div")
-//     // position: "absolute"
-//     });
-
-// $("div.blast").each(function(index, element) {
-// // 	console.log(index);
-// // 	// // $( this).addClass( index);
-// // 	// console.log($(this));
-// // 	// console.log(element.getBoundingClientRect());
-// // 	// $( this).style.position("absolute");
-// // 	// if(index == 1){
-// 			// $( this).position( {
-// 			// 	position:"absolute",
-//  		// 		 top:0,
-// 			// 	 right:0,
-// 			// 	 width:"200px",
-//  		// 		});
-// // 	// 	}
-// 	if(index %2 == 0){
-// // 		// $( this).velocity("slideDown", { 
-// // 	 // 		duration: 1 })
-
-// // 		// 	.velocity("slideUp", { 
-// // 	 //    	duration: 1500 
-	    	
-// // 	 //    });
-
-		
-// 		$(this).velocity({
-//    			 translateY: "20px",
-//    			 rotateZ: "20deg"
-// 		});
-
-// 	}
-// });
-
-
-
-// ANIMATED DELETION
-// animatedDelete()
-
-
-// function animatedDelete(){
-//     $("span").each(function(index, element) {
-
-// 	if(index %2 == 0){
-// 		$( this).slideDown({ 
-// 	 		duration: 100 ,
-// 	 		delay: 1000})
-
-// 			.slideUp( "slow", function() {
-//    				 console.log("animation complete");
-//    				 element.style.display = "inline";
-
-//   				 var c = $("body").css("background-color");
-// 				 var colors = c.replace(/^rgba?\(|\s+|\)$/g,'').split(',');
-// 				 for(var i in colors) {
-// 				  console.log(colors[i]);
-// 				 }
-// 				 background_rgb[0] = colors[0];
-// 				 background_rgb[1] = colors[1];
-// 				 background_rgb[2] = colors[2];
-// 				 var backgroundWrite = "rgb(" + background_rgb[0] + ", " + background_rgb[1] + ", " + background_rgb[2] + ")";
-//    				 $( this).css("color" , backgroundWrite);
-   				 
-//  			 });
-
-// 	}
-// }); 
-// }
-
-
-
-
-
-
-
-
-
-
